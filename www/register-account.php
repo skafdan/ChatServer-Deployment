92e70a3f294353fd3322c0094a24213fc62323a4
Dan <dan@danskaf.dev>
2021-09-24 10:29:38 +1200

    Updates RDS credentials, fixes table name in query
    Signed-off-by: Dan <dan@danskaf.dev>

--- www/register-account.php
+++ www/register-account.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = "chatserverdb.cd3ixmxbmpj0.us-east-1.rds.amazonaws.com";
+$dbhost = "chatlogs.ctqfgoekb0ox.us-east-1.rds.amazonaws.com";
 $dbport = 3306;
-$dbname = "chatserverdb";
+$dbname = "chatserver";
 $db_username = "admin";
-$db_password = "Quack1nce4^";
+$db_password = "quack1nce4^";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM users WHERE username=$username";
+$query = "SELECT * FROM user WHERE username=$username";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
@@ -24,7 +24,7 @@ if ($result->num_rows > 0) {
     header('Location: register-failed.php');
 }
 
-$query = "INSERT INTO users VALUES ($username, $password, $salt)";
+$query = "INSERT INTO user VALUES ($username, $password, $salt)";
 $result = $conn->query($query);
 
 $conn->close();
