c0f5c05943817a5ee752aaf927b3c00ea3df3fdc
Hayden McAlister <haydenmcalister49@gmail.com>
2021-09-24 13:59:39 +1200

    Add python script to upload a file to s3 bucket

---
+++ backup_to_bucket.py
@@ -0,0 +1,8 @@
+import boto3
+import datetime
+
+# Requires existence of ~/.aws/credentials ?
+s3 = boto3.client('s3')
+with open("table_data.csv", "rb") as f:
+    current_datetime = f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    s3.upload_fileobj(f, "BUCKET_NAME", "BACKUP_"+current_datetime)
